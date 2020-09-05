a32f746e9082f8c90ace27ba415005ce53623c13
Raphael Ndonga <nkibi53@gmail.com>
2020-09-05 13:33:57 +0300

    Starting off the gads project with a bang

---
+++ app/src/main/java/com/example/android/gadspracticeproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.gadspracticeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        setContentView(R.layout.activity_main)
+    }
+}
